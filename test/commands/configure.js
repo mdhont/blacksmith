e845c229c32e8e7544d0e1fad454ff081e7f8d12
Andres <andres@bitnami.com>
2017-02-24 16:07:54 +0100

    Renamed test files

--- test/commands/configure.js
+++ test/commands/configure.js
@@ -2,20 +2,14 @@
 
 const _ = require('lodash');
 const fs = require('fs');
-const path = require('path');
 const chai = require('chai');
 const expect = chai.expect;
-const helpers = require('./helpers');
+const helpers = require('../helpers');
 const BlacksmithHandler = helpers.Handler;
 const blacksmithHandler = new BlacksmithHandler();
-const spawnSync = require('child_process').spawnSync;
-const configFile = path.join(__dirname, '../config.json');
-
 
 describe('#configure', function() {
   this.timeout(20000);
-  beforeEach(() => spawnSync('cp', [configFile, `${configFile}.bk`]));
-  afterEach(() => spawnSync('mv', [`${configFile}.bk`, configFile]));
   it('Throw error when called with wrong arguments', function() {
     expect(() => blacksmithHandler.exec('configure')).to.throw('expects at least 1 argument');
     expect(
@@ -25,35 +19,44 @@ describe('#configure', function() {
       () => blacksmithHandler.exec('configure --action add paths.recipes')
     ).to.throw('You need to specify a value');
   });
-  function check(property, value) {
+  function check(configFile, property, value) {
     const config = JSON.parse(fs.readFileSync(configFile, {encoding: 'utf-8'}));
     const actualValue = _.get(config, property);
     return _.isEqual(actualValue, value);
   }
-  function get(property) {
+  function get(configFile, property) {
     const config = JSON.parse(fs.readFileSync(configFile, {encoding: 'utf-8'}));
     return _.get(config, property);
   }
+  afterEach(helpers.cleanTestEnv);
   it('It set a string property', function() {
-    blacksmithHandler.javascriptExec(path.join(__dirname, '../index.js'),
+    const test = helpers.createTestEnv();
+    blacksmithHandler.javascriptExec(
+      test.configFile,
       'configure compilation.prefix /tmp/test');
-    expect(check('compilation.prefix', '/tmp/test')).to.be.eql(true);
+    expect(check(test.configFile, 'compilation.prefix', '/tmp/test')).to.be.eql(true);
   });
   it('It set an array property', function() {
-    blacksmithHandler.javascriptExec(path.join(__dirname, '../index.js'),
+    const test = helpers.createTestEnv();
+    blacksmithHandler.javascriptExec(
+      test.configFile,
       'configure --action set paths.recipes /tmp/test');
-    expect(check('paths.recipes', ['/tmp/test'])).to.be.eql(true);
+    expect(check(test.configFile, 'paths.recipes', ['/tmp/test'])).to.be.eql(true);
   });
 
   it('It adds a value to an array property', function() {
-    const previousValue = get('paths.recipes');
-    blacksmithHandler.javascriptExec(path.join(__dirname, '../index.js'),
+    const test = helpers.createTestEnv();
+    const previousValue = get(test.configFile, 'paths.recipes');
+    blacksmithHandler.javascriptExec(
+      test.configFile,
       'configure --action add paths.recipes /tmp/test');
-    expect(check('paths.recipes', previousValue.concat('/tmp/test'))).to.be.eql(true);
+    expect(check(test.configFile, 'paths.recipes', previousValue.concat('/tmp/test'))).to.be.eql(true);
   });
   it('It unset a property', function() {
-    blacksmithHandler.javascriptExec(path.join(__dirname, '../index.js'),
+    const test = helpers.createTestEnv();
+    blacksmithHandler.javascriptExec(
+      test.configFile,
       'configure --action unset compilation.prefix');
-    expect(get('compilation.prefix')).to.be.eql(null);
+    expect(get(test.configFile, 'compilation.prefix')).to.be.eql(null);
   });
 });
