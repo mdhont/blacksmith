a1a4073fb9564af325e94415f5e9f0a0c0f158d0
Andres <andres.mgotor@gmail.com>
2017-06-08 16:07:59 +0200

    Use tar library instead tarballUtils when compressing artifacts (#43)
    * Use tar library instead tarballUtils when compressing artifacts
    
    Summary:
    The problem with the tar method of tarball utils is that if the list of files to compress is too big it throws an error.
    We reduced the amount of files passed to tarballUtils.tar but that slowed down the process a lot.
    
    Instead of that we are now using the 'tar' library for node since it allowes any amount of files as arguments. To adapt
    the code to 'tar' we needed:
     - Use relative paths when calling for compression
     - Exclude the files beforehand. `tar.c` doesn't support to exclude files.
    
    Reviewers: jfuentes
    
    Differential Revision: http://phabricator.bitnami.com:8080/D16359

--- test/core/fstracker.js
+++ test/core/fstracker.js
@@ -3,9 +3,7 @@
 const FileSystemTracker = require('../../lib/core/build-manager/artifacts/fstracker');
 const path = require('path');
 const fs = require('fs');
-const sinon = require('sinon');
 const spawnSync = require('child_process').spawnSync;
-const tarballUtils = require('tarball-utils');
 const chai = require('chai');
 const chaiFs = require('chai-fs');
 const expect = chai.expect;
@@ -76,66 +74,6 @@ describe('FSTracker', () => {
     const tarball = '/tmp/blacksmith-test-env/delta.tar.gz';
     fstracker.captureDelta(tarball, {all: true});
     expect(spawnSync('tar', ['-ztf', tarball]).stdout.toString()).to.contain('hello_dir');
-  });
-  it('calls for compression using just the directory paths', () => {
-    const fstracker = new FileSystemTracker(testDir);
-    fstracker.init();
-    fs.mkdirSync(path.join(testDir, 'directory'));
-    fs.writeFileSync(path.join(testDir, 'directory/hello'), 'hello');
-    fs.writeFileSync(path.join(testDir, 'directory/hello2'), 'hello');
-    fstracker.commit();
-    const tarball = '/tmp/blacksmith-test-env/delta.tar.gz';
-    let result = [];
-    sinon.stub(tarballUtils, 'tar').callsFake(list => result = list);
-    try {
-      fstracker.captureDelta(tarball, {all: true});
-    } catch (e) {
-      tarballUtils.tar.restore();
-      throw e;
-    }
-    tarballUtils.tar.restore();
-    expect(result).to.be.eql([path.join(testDir, 'directory')]);
-  });
-  it('calls for compression using just the minimum number of directories', () => {
-    const fstracker = new FileSystemTracker(testDir);
-    fstracker.init();
-    fs.writeFileSync(path.join(testDir, 'a'), 'hello');
-    fs.mkdirSync(path.join(testDir, 'b'));
-    fs.writeFileSync(path.join(testDir, 'b/c'), 'hello');
-    fs.mkdirSync(path.join(testDir, 'b/d'));
-    fs.writeFileSync(path.join(testDir, 'b/d/e'), 'hello');
-    fstracker.commit();
-    const tarball = '/tmp/blacksmith-test-env/delta.tar.gz';
-    let result = [];
-    sinon.stub(tarballUtils, 'tar').callsFake(list => result = list);
-    try {
-      fstracker.captureDelta(tarball, {all: true});
-    } catch (e) {
-      tarballUtils.tar.restore();
-      throw e;
-    }
-    tarballUtils.tar.restore();
-    expect(result).to.be.eql([path.join(testDir, 'b'), path.join(testDir, 'a')]);
-  });
-  it('calls for compression using just the minimum number of directories without missing files', () => {
-    const fstracker = new FileSystemTracker(testDir);
-    fstracker.init();
-    fs.writeFileSync(path.join(testDir, 'a'), 'hello');
-    fs.mkdirSync(path.join(testDir, 'b'));
-    fs.writeFileSync(path.join(testDir, 'b/c'), 'hello');
-    fs.writeFileSync(path.join(testDir, 'bb'), 'hello');
-    fstracker.commit();
-    const tarball = '/tmp/blacksmith-test-env/delta.tar.gz';
-    let result = [];
-    sinon.stub(tarballUtils, 'tar').callsFake(list => result = list);
-    try {
-      fstracker.captureDelta(tarball, {all: true});
-    } catch (e) {
-      tarballUtils.tar.restore();
-      throw e;
-    }
-    tarballUtils.tar.restore();
-    expect(result).to.be.eql([path.join(testDir, 'b'), path.join(testDir, 'a'), path.join(testDir, 'bb')]);
   });
   it('captures a selection of files', () => {
     const fstracker = new FileSystemTracker(testDir);
